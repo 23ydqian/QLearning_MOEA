1b44a8553af24a6aeb66eb82b04e625c74cfbc3b
qyd <qyd>
2024-10-29 14:49:38 +0800

    10.29

--- Other/HLS/DSpoint.py
+++
@@ -1,8 +0,0 @@
-class DSpoint:
-
-    def __init__(self, latency, area, time,configuration):
-        self.area = area
-        self.latency = latency
-        self.time = time
-        self.configuration = configuration
-        self.isSynthesis = 0
